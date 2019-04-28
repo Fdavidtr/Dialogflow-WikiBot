6a14d704da04206fdd688aa8264f616ce39dc34a
David TR <dtrujillo@singularfactory.com>
2019-04-28 21:47:01 +0100

    funcionando inicialmente

--- functions/index.js
+++ functions/index.js
@@ -14,15 +14,24 @@ const linkUrl = 'https://assistant.google.com/';
 
 const axios = require('axios')
 
-function urlify(text) {
-  const key = text.search('class="r"')
-  return text.substring(key, key+ 256)
-  var urlRegex = /(https?:\/\/[^\s]+)/g;
-  return text.replace(urlRegex, function(url) {
-    return url;
-  })
-  // or alternatively
-  // return text.replace(urlRegex, '<a href="$1">$1</a>')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function extractResultFromHtml(text) {
+  //const key = text.search('class="r"')
+  //return text.substring(key, key+ 256)
+
+  const { window } = new JSDOM(text);
+  const headerSection = window.document.querySelector('.r')
+  const description = window.document.querySelector('.st')
+  let url = window.document.querySelector('.r a').href//.match(/http(.*)/)
+  if(url.match(/http(.*)/)) url = url.match(/http(.*)/)[0]
+  else if(url[0] === '/') url = 'https://www.google.com' + url
+  return {
+    title: headerSection.textContent,
+    url: url,
+    desc: description.textContent
+  }
 }
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
@@ -35,14 +44,20 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   }
 
   async function fallback(agent) {
-    agent.add(`No te entiendo viejor`);
     const queryFormatted = request.body.queryResult.queryText.trim()
     agent.add(`${queryFormatted} ???`);
     const resp = await axios.get(`https://www.google.com/search`,{params:{q:queryFormatted}})
+    const fullRequestUrl = resp.request.res.req.agent.protocol+"//"+resp.request.res.connection._host+resp.request.path
+    agent.add(`${fullRequestUrl}`)
     const result = resp.data
-    const jsonResult = urlify(result)
-
+    const jsonResult = extractResultFromHtml(result)
     console.log(jsonResult)
+    for (const key in jsonResult){
+      let val = (jsonResult[key]||'')
+      console.log(val)
+      agent.add(`${key}: ${val}`)
+    }
+
   }
 
   // Run the proper handler based on the matched Dialogflow intent
