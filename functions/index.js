c11f2b759bbcc4e5b3826b2a8cffa99b648123f6
David TR <dtrujillo@singularfactory.com>
2019-04-28 19:21:40 +0100

    incremental

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,53 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card, Suggestion } = require('dialogflow-fulfillment');
+const { Carousel } = require('actions-on-google');
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+// URLs for images used in card rich responses
+const imageUrl = 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png';
+const imageUrl2 = 'https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw';
+const linkUrl = 'https://assistant.google.com/';
+
+const axios = require('axios')
+
+function urlify(text) {
+  const key = text.search('class="r"')
+  return text.substring(key, key+ 256)
+  var urlRegex = /(https?:\/\/[^\s]+)/g;
+  return text.replace(urlRegex, function(url) {
+    return url;
+  })
+  // or alternatively
+  // return text.replace(urlRegex, '<a href="$1">$1</a>')
+}
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  //console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  //console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  async function fallback(agent) {
+    agent.add(`No te entiendo viejor`);
+    const queryFormatted = request.body.queryResult.queryText.trim()
+    agent.add(`${queryFormatted} ???`);
+    const resp = await axios.get(`https://www.google.com/search`,{params:{q:queryFormatted}})
+    const result = resp.data
+    const jsonResult = urlify(result)
+
+    console.log(jsonResult)
+  }
+
+  // Run the proper handler based on the matched Dialogflow intent
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  agent.handleRequest(intentMap);
+});
